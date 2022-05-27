209a9314b3b570f15902e3bfe08f50ce4122a999
Basia <bao.zajac@gmail.com>
2022-05-27 19:06:48 +0200

    poprawienie/dodanie opisu metod, dodanie daty ostatnie noclegu do danych, oblicznie il. dni od ost noclegu

--- uczestnicy.py
+++ uczestnicy.py
@@ -1,4 +1,6 @@
 from main import read_db
+import datetime
+from datetime import datetime
 
 
 class Pielgrzymi:
@@ -21,15 +23,18 @@ class Pielgrzymi:
 
         self.wszyscy_pielgrzymi = self.funkcyjni_0 + self.funkcyjni_1 + self.funkcyjni_2 + self.funkcyjni_szkola \
                                   + self.pielgrzymi_bez_noclegu + self.pielgrzymi_pozostali
+        self.wszyscy_pielg_zwykli = self.pielgrzymi_bez_noclegu + self.pielgrzymi_pozostali
+
 
     def funkcyjny(self):
-        # dane funkcyjnego: id, funkcja, płeć, priorytet
+        # dane funkcyjnego: id, funkcja, płeć, ostatni nocleg, priorytet
         for id_p, dane_p in self.dane.items():
             if dane_p[2] == "funkcyjni":
                 self.id_pielgrzyma = id_p
                 self.funkcja_pielgrzyma = dane_p[3]
                 self.plec = dane_p[5]
-                dane_funkcyjnego = [self.id_pielgrzyma, self.funkcja_pielgrzyma, self.plec]
+                self.data_nocl = dane_p[4]
+                dane_funkcyjnego = [self.id_pielgrzyma, self.funkcja_pielgrzyma, self.plec, self.data_nocl]
                 if self.funkcja_pielgrzyma == "porządkowy" or self.funkcja_pielgrzyma == "chorąży":
                     self.funkcyjni_0.append(dane_funkcyjnego)
                 elif self.funkcja_pielgrzyma == "szef" or self.funkcja_pielgrzyma == "pilot" \
@@ -41,27 +46,28 @@ class Pielgrzymi:
                     self.funkcyjni_2.append(dane_funkcyjnego)
 
     def podaj_func(self):
-        print("DANE FUNKCYJNEGO: id, funkcja, płeć, priorytet\n")
+        print("DANE FUNKCYJNEGO: id, funkcja, płeć, ostatni nocleg, priorytet\n")
         print(f"funkcyjni z grupy 0: {self.funkcyjni_0}")
         print(f"funkcyjni z grupy 1: {self.funkcyjni_1}")
         print(f"funkcyjni z grupy 2: {self.funkcyjni_2}")
         print(f"funkcyjni z grupy do szkoły: {self.funkcyjni_szkola}")
 
     def zwykly_pielgrzym(self):
-        # dane pielgrzyma: id, nr grupki, płeć, priorytet
+        # dane pielgrzyma: id, nr grupki, płeć, ostatni nocleg, priorytet
         for id_p, dane_p in self.dane.items():
             if dane_p[2] != "funkcyjni":
                 self.id_pielgrzyma = id_p
                 self.grupka_pielgrzyma = dane_p[2]
                 self.plec = dane_p[5]
-                dane_zwyk_pielgrzyma = [self.id_pielgrzyma, self.grupka_pielgrzyma, self.plec]
+                self.data_nocl = dane_p[4]
+                dane_zwyk_pielgrzyma = [self.id_pielgrzyma, self.grupka_pielgrzyma, self.plec, self.data_nocl]
                 if 0 > 2:                       # TODO: napisać warunek dla: sa_bez_noclegu >= 3 dni
                     self.pielgrzymi_bez_noclegu.append(dane_zwyk_pielgrzyma)
                 else:
                     self.pielgrzymi_pozostali.append(dane_zwyk_pielgrzyma)
 
     def podaj_zwyk_pielg(self):
-        print("DANE PIELGRZYMA: id, nr grupki, płeć, priorytet\n")
+        print("DANE PIELGRZYMA: id, nr grupki, płeć, ostatni nocleg, priorytet\n")
         print(f"Pielgrzymi, bez noclegu od 3 dni: {self.pielgrzymi_bez_noclegu}")
         print(f"Pozostali pielgrzymi: {self.pielgrzymi_pozostali}")
 
@@ -118,13 +124,23 @@ class Pielgrzymi:
         # print(self.funkcyjni_0)
 
     def podsum_il_wg_prioryt(self):
-        # zestawienie priorytetów [priorytet: ilość]
+        # zestawienie priorytetów /priorytet: ilość/
         for el in self.wszyscy_pielgrzymi:
-            if not self.suma_priorytetow.get(el[3]):
-                self.suma_priorytetow[el[3]] = 0
-            self.suma_priorytetow[el[3]] += 1
+            if not self.suma_priorytetow.get(el[4]):
+                self.suma_priorytetow[el[4]] = 0
+            self.suma_priorytetow[el[4]] += 1
         print(self.suma_priorytetow)
         print(sorted(self.suma_priorytetow.items()))
+
+    # obliczanie ilości dni od ostatniego noclegu dla wszystkich z danej grupy na raz
+    def delta_ostatni_nocleg(self):
+        today = datetime.now().date()
+        for el in self.wszyscy_pielg_zwykli:
+            last_accommodation = datetime.strptime(el[3], "%d-%m-%Y").date()
+            delta_nocleg_obl = today - last_accommodation
+            self.delta_nocleg = delta_nocleg_obl.days
+            print(self.delta_nocleg)
+
 
     def borderer(func):             # TODO: jak zrobić dekorator wewnątrz klasy?
         print("------------------")
@@ -132,18 +148,20 @@ class Pielgrzymi:
 
 
 pielg = Pielgrzymi("pielgrzymi.json")
-print("----------------------------------------")
-pielg.podaj_func()
-print("----------------------------------------")
-pielg.podaj_zwyk_pielg()
-print("----------------------------------------")
+# print("----------------------------------------")
+# pielg.podaj_func()
+# print("----------------------------------------")
+# pielg.podaj_zwyk_pielg()
+# print("----------------------------------------")
+# # print()
+# print("PODSUMOWANIE LICZEBNOŚCI GRUP")
+# pielg.podsum_wszyst_plcie()
 # print()
-print("PODSUMOWANIE LICZEBNOŚCI GRUP")
-pielg.podsum_wszyst_plcie()
-print()
-pielg.podsum_il_w_grupach()
-print()
-print("zestawienie priorytetów [priorytet: ilość]")
-pielg.podsum_il_wg_prioryt()
+# pielg.podsum_il_w_grupach()
+# print()
+# print("zestawienie priorytetów [priorytet: ilość]")
+# pielg.podsum_il_wg_prioryt()
+
+pielg.delta_ostatni_nocleg()
 
 
