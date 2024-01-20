1b3a023149b384b9c3f4f4e2b82ed048e3d34f06
Basia <bao.zajac@gmail.com>
2024-01-20 22:42:10 +0100

    remove unused methods

--- pilgrim/pilgrims.py
+++ pilgrim/pilgrims.py
@@ -49,13 +49,6 @@ class Pilgrims:
                 else:
                     self.role_2.append(data_role)
 
-    def give_role(self):
-        print("DANE FUNKCYJNEGO: id, funkcja, płeć, ostatni nocleg, priorytet\n")
-        print(f"funkcyjni z grupy 0: {self.role_0}")
-        print(f"funkcyjni z grupy 1: {self.role_1}")
-        print(f"funkcyjni z grupy 2: {self.role_2}")
-        print(f"funkcyjni z grupy do szkoły: {self.role_school}")
-
     def normal_pilgrim(self):
         # pilgrim data: id, no. of a small group, sex, last accommodation, priority
         self.small_groups_division = {}
@@ -80,11 +73,6 @@ class Pilgrims:
                 else:
                     self.pilgrims_other.append(data_normal_pilgrim)
 
-    def give_normal_pilgr(self):
-        print("DANE PIELGRZYMA: id, nr grupki, płeć, ostatni nocleg, priorytet\n")
-        print(f"Pielgrzymi, bez noclegu od 3 dni: {self.pilgrims_no_accommod}")
-        print(f"Pozostali pielgrzymi: {self.pilgrims_other}")
-
     def priority_sex(self, grp, priority):
         for el in grp:
             self.priority = priority
@@ -103,44 +91,6 @@ class Pilgrims:
     def normal_pilgr_priority(self):
         self.priority_sex(self.pilgrims_no_accommod, 4)
         self.priority_sex(self.pilgrims_other, 8)
-
-    def sum_ppl_in_group(self, grp, grp_name):
-        women_number = 0
-        men_number = 0
-        for el in grp:
-            if el[2] == "kobieta":
-                women_number += 1
-            else:
-                men_number += 1
-        all = women_number + men_number
-        # print(f" Grupa '{grp_name}' >>  razem: {all}, k: {women_number}, m: {men_number}")
-        return grp_name, all, women_number, men_number
-
-    def summary_number_in_groups(self):
-        self.sum_ppl_in_group(self.role_0, "funkcyjni_0")
-        self.sum_ppl_in_group(self.role_1, "funkcyjni_1")
-        self.sum_ppl_in_group(self.role_2, "funkcyjni_2")
-        self.sum_ppl_in_group(self.role_school, "funkcyjni_szkola")
-        self.sum_ppl_in_group(self.pilgrims_no_accommod, "pielgrzymi_bez_noclegu")
-        self.sum_ppl_in_group(self.pilgrims_other, "pielgrzymi_pozostali")
-
-    def summary_number_in_small_groups(self):
-        print(self.small_groups_division.items())
-        for k, v in self.small_groups_division.items():
-            a = self.sum_ppl_in_group(v, k)
-        return a
-
-    def summary_number_sex(self):
-        # self.sum_ppl_in_group(self.all_pilgrims, "wszyscy_razem")
-        return self.sum_ppl_in_group(self.all_pilgrims, "wszyscy_razem")
-
-    def summary_number_priority(self):
-        # catalog of priorities /priority: amount/
-        for el in self.all_pilgrims:
-            if not self.sum_priority.get(el[4]):
-                self.sum_priority[el[4]] = 0
-            self.sum_priority[el[4]] += 1
-        return self.sum_priority
 
     # count no. of days from the last accommodation
     def from_last_accommod(self, date_day):
